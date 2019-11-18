338170384160ee9509ad948b14df41a246960f44
Brandon <bchen00@stuy.edu>
2019-11-18 04:57:23 +0000

    fixed seg fault

--- direct.c
+++ direct.c
@@ -12,26 +12,30 @@ int main(){
     printf("%s\n", strerror(errno));
   }
 
-  char allDir[] = "";
-  char allFiles[] = "";
+  int size = 0;
+  char *allDir = malloc(0);
+  char *allFiles = malloc(0);
   struct dirent *entry = readdir(stream);
   while (entry != NULL){
-    char curr[] = "";
+    char *curr = malloc(0);
     strcat(curr,entry->d_name);
 
     if (entry->d_type == DT_DIR){
-      //strcat(allDir,curr);
-      //strcat(allDir,"\n");
+      strcat(allDir,"\t");
+      strcat(allDir,curr);
+      strcat(allDir,"\n");
     }
     else{
-      //strcat(allFiles,curr);
-      //strcat(allFiles,"\n");
+      strcat(allFiles,"\t");
+      strcat(allFiles,curr);
+      strcat(allFiles,"\n");
       //size goes here
     }
     entry = readdir(stream);
   }
-  printf("%s", allDir);
-  //printf("%s", allFiles);
+  printf("All Directories:\n%s", allDir);
+  printf("\n");
+  printf("All Regular Files:\n%s", allFiles);
 
   closedir(stream);
 
