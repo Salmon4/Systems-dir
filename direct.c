98a7347e656ff19e6da83297e736b0bfda27b875
Brandon <bchen00@stuy.edu>
2019-11-18 23:44:21 +0000

    attempted finding size of subdirectories. Formatted size

--- direct.c
+++ direct.c
@@ -6,6 +6,29 @@
 #include <string.h>
 #include <sys/stat.h>
 
+int findDirSize(char *path){
+  DIR * stream = opendir(path);
+  int size = 0;
+  struct dirent *entry = readdir(stream);
+  while (entry != NULL){
+    char *curr = malloc(0);
+    strcat(curr,entry->d_name);
+    if (entry->d_type == DT_DIR){
+      strcat(path,"/");
+      strcat(path,entry->d_name);
+      size += findDirSize(path);
+    }
+    else{
+      struct stat buffer;
+      char *file = entry->d_name;
+      stat(file,&buffer);
+      size += buffer.st_size;
+    }
+    entry = readdir(stream);
+  }
+  return size;
+}
+
 int main(){
   DIR * stream = opendir(".");
 
@@ -25,6 +48,13 @@ int main(){
       strcat(allDir,"\t");
       strcat(allDir,curr);
       strcat(allDir,"\n");
+      //char *path = malloc(0);
+      //strcat(path,"/");
+      //strcat(path,entry->d_name);
+      //if (strcmp(entry->d_name,"/.") != 0){
+      //  printf("%s\n",path);
+      //  size += findDirSize(path);
+      //}
     }
     else{
       strcat(allFiles,"\t");
@@ -39,7 +69,29 @@ int main(){
     entry = readdir(stream);
   }
 
-  printf("The size is %d\n\n", size);
+  char format[] = " ";
+  if (size > 1073741823){
+    size = size / 1073741824;
+    strcat(format,"GB");
+    //char format[] = "GB";
+  }
+  else if (size > 1048575){
+    size = size / 1048576;
+    strcat(format,"MB");
+    //char format[] = "MB";
+  }
+  else if (size > 1023){
+    size = size / 1024;
+    strcat(format,"KB");
+    //char format[] = "KB";
+  }
+  else{
+    strcat(format,"B");
+    //char format[] = "B";
+  }
+
+
+  printf("The size is %d%s\n\n", size,format);
   printf("All Directories:\n%s", allDir);
   printf("\n");
   printf("All Regular Files:\n%s", allFiles);
